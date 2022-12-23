51af52722580b4f4a85e7d726d0360baa45581d6
Andrea Colussi <andrea.colussi@external.fcagroup.com>
2022-12-23 17:44:02 +0100

    opponentWonMatch did

--- Presentation/BoardScreen/ViewModels/Models/GameInputViewModel.swift
+++ Presentation/BoardScreen/ViewModels/Models/GameInputViewModel.swift
@@ -11,12 +11,12 @@ import Combine
 
 final class GameInputViewModel {
     private var currentBoard : Board
-    private var boardFactory : BoardFactoryInterface?
+    private var boardFactory : BoardFactoryInterface
     
     var viewControllerProtocol : BoardViewControllerProtocol?
     let defaultBoard = Board(cells: [[]], player: Player.startPlayerValue, opponent: Player.startOpponentValue, drawMode: .normal)
     
-    init(boardFactory : BoardFactoryInterface?){
+    init(boardFactory : BoardFactoryInterface){
         self.boardFactory = boardFactory
         currentBoard = defaultBoard
     }
@@ -29,35 +29,38 @@ extension GameInputViewModel : PresentationLayerInputListenerInterface {
     }
     
     func opponentQuitMatch(game: Game) {
-        //TODO: handle
+        viewControllerProtocol?.handelEvent(gameEvent: .endGame)
     }
     
     func opponentWonMatch(game: Game) {
-        //TODO: handle
+        let newPosition = game.lastMove.pawnMove
+        let wrapper = boardFactory.getBoardCellsFromPawn(newMove: newPosition)
+        currentBoard = wrapper.updatedBoard
+        viewControllerProtocol?.updateOpponentPawn(start: wrapper.startPosition, destination: wrapper.endPosition)
+        viewControllerProtocol?.handelEvent(gameEvent: .matchLost)
     }
     
     func opponentMadeMove(game: Game) {
-        let stage = boardFactory?.gameState(game: game) ?? .noMove
+        let stage = boardFactory.gameState(game: game)
         switch stage {
         case .gameAlreadyStarted:
-            if let b = boardFactory?.updateBoard(game: game){
-                currentBoard = b
-                viewControllerProtocol?.initBoard(board: b)
-            }
+            currentBoard = boardFactory.updateBoard(game: game)
+            viewControllerProtocol?.initBoard(board: currentBoard)
+            
             
         case .opponentMovePawn:
             let newPosition = game.lastMove.pawnMove
-            if let wrapper = boardFactory?.getBoardCellsFromPawn(newMove: newPosition) {
-                currentBoard = wrapper.updatedBoard
-                viewControllerProtocol?.updateOpponentPawn(start: wrapper.startPosition, destination: wrapper.endPosition)
-            }
+            let wrapper = boardFactory.getBoardCellsFromPawn(newMove: newPosition)
+            currentBoard = wrapper.updatedBoard
+            viewControllerProtocol?.updateOpponentPawn(start: wrapper.startPosition, destination: wrapper.endPosition)
+            
             
         case .opponentInsertWall:
             let newWall = game.lastMove.wallMove
-            if let wrapper = boardFactory?.getBoardCellsFromWall(newWall: newWall) {
-                currentBoard = wrapper.updatedBoard
-                viewControllerProtocol?.updateWall(topRight: wrapper.topRight, topLeft: wrapper.topLeft, bottomRight: wrapper.bottomRight, bottomLeft: wrapper.bottomLeft)
-            }
+            let wrapper = boardFactory.getBoardCellsFromWall(newWall: newWall)
+            currentBoard = wrapper.updatedBoard
+            viewControllerProtocol?.updateWall(topRight: wrapper.topRight, topLeft: wrapper.topLeft, bottomRight: wrapper.bottomRight, bottomLeft: wrapper.bottomLeft)
+            
         case .noMove:
             break
         }
