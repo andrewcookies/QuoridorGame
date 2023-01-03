06b748cb3e4e3d419d8ad79913e6e1f67ade7a71
Andrea Colussi <andrea.colussi@external.fcagroup.com>
2023-01-03 12:01:05 +0100

    const for default values

--- Domain/Utilities/BoardUIVariables.swift
+++ Domain/Utilities/BoardUIVariables.swift
@@ -33,3 +33,11 @@ let topCellsBorder = [0,1,2,3,4,5,6,7,8,9]
 let bottomCellsBorder = [80,81,82,83,84,85,86,87,88,89]
 let leftCellsBorder = [0,10,20,30,40,50,60,70,80]
 let rightCellsBorder = [8,18,28,38,48,58,68,78,88]
+
+/*
+ Default values
+ */
+
+let defaultPlayerName = "- -"
+let defaultPlayerId = "id_nil"
+
