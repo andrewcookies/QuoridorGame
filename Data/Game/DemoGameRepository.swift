5f05fd7060af0d8796abbc0a73b462b501d20ec5
Andrea Colussi <andrea.colussi@external.fcagroup.com>
2023-01-03 11:57:23 +0100

    Refactor DemoMode

--- Data/Game/DemoGameRepository.swift
+++ Data/Game/DemoGameRepository.swift
@@ -9,30 +9,34 @@ import Foundation
 
 class DemoGameOutputRepository : GameRepositoryOutputInterface {
     
-    var demoGame = Game.defaultValue
+    var demoGame : Game?
     func updateState(state: GameState) async throws {
-        let game = Game(created: demoGame.created,
-                        state: state,
-                        player1: demoGame.player1,
-                        player2: demoGame.player2,
-                        lastMove: demoGame.lastMove,
-                        gameMoves: demoGame.gameMoves)
-        demoGame = game
-        NotificationCenter.default.post(name: Notification.Name("update"), object: nil)
+        if let g = demoGame {
+            let game = Game(created: g.created,
+                            state: state,
+                            player1: g.player1,
+                            player2: g.player2,
+                            lastMove: g.lastMove,
+                            gameMoves: g.gameMoves)
+            demoGame = g
+            NotificationCenter.default.post(name: Notification.Name("update"), object: nil)
+        }
     }
     
     func updateGame(game: Game) async throws {
-        let demoMove = Move(playerId: "DEMO_PLAYER", pawnMove: Pawn.startValue, wallMove:  Wall.initValue, moveType: .movePawn)
-        var moves = demoGame.gameMoves
-        moves.append(demoMove)
-        let newGame = Game(created: game.created,
-                           state: .inProgress,
-                           player1: game.player1,
-                           player2: game.player2,
-                           lastMove: demoMove,
-                           gameMoves: moves)
-        demoGame = newGame
-        NotificationCenter.default.post(name: Notification.Name("update"), object: nil)
+        if let g = demoGame {
+            let demoMove = Move(playerId: "DEMO_PLAYER", pawnMove: Pawn.startValue, wallMove:  Wall.initValue, moveType: .movePawn)
+            var moves = g.gameMoves
+            moves.append(demoMove)
+            let newGame = Game(created: g.created,
+                               state: .inProgress,
+                               player1: g.player1,
+                               player2: g.player2,
+                               lastMove: demoMove,
+                               gameMoves: moves)
+            demoGame = newGame
+            NotificationCenter.default.post(name: Notification.Name("update"), object: nil)
+        }
     }
     
   
@@ -49,7 +53,9 @@ class DemoGameInputRepository : DemoGameOutputRepository, GameRepositoryInputInt
     }
     
     @objc func update(){
-        inputUseCase.updateGameFromOpponent(game: demoGame)
+        if let g = demoGame {
+            inputUseCase.updateGameFromOpponent(game: g)
+        }
     }
     
     func searchOpenMatch() async throws -> String {
@@ -58,20 +64,20 @@ class DemoGameInputRepository : DemoGameOutputRepository, GameRepositoryInputInt
     }
     
     func joinMatch(player: Player, gameId: String) async throws -> Game {
-        
+        guard let g = demoGame else { throw APIError.currentInfoNIL }
         let demoMove = Move(playerId: "DEMO_PLAYER", pawnMove: Pawn.startValue, wallMove:  Wall.initValue, moveType: .movePawn)
         let move = Move(playerId: player.playerId, pawnMove: player.pawnPosition, wallMove: player.walls.last ?? Wall.initValue, moveType: .movePawn)
-        var moves = demoGame.gameMoves
+        var moves = g.gameMoves
         moves.append(demoMove)
         moves.append(move)
-        let newGame = Game(created: demoGame.created,
+        let newGame = Game(created: g.created,
                            state: .inProgress,
-                           player1: demoGame.player1,
+                           player1: g.player1,
                            player2: player,
                            lastMove: move,
                            gameMoves: moves)
         demoGame = newGame
-        return demoGame
+        return newGame
     }
     
     func createMatch(game: Game) async throws -> Game {
